41a29f46d8347f0aa9e56203462422110503ac73
neelbamroliya <neelbamroliya0@gmail.com>
2023-02-11 01:54:40 +0530

    ended ....

---
+++ server/model/User.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "please provide unique username"],
+    unique: [true, "username already exist"]
+  },
+  password: {
+    type: String,
+    required: [true, "please provide password"],
+    unique: false
+  },
+  email: {
+    type: String,
+    required: [true, "please provide a unique email"],
+    unique: true
+  },
+  firstname: { type: String },
+  lastname: { type: String },
+  mobile: { type: Number },
+  address: { type: String },
+  profile: { type: String }
+})
+
+export default mongoose.model.Users || mongoose.model("User", UserSchema)
